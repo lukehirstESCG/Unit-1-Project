b4df22294b7f01532af40d52ebe9f3def5d88b1e
Luke Hirst <521480@chrome.escg.ac.uk>
2022-11-06 17:06:01 +0000

    Unhid all movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,50 +21,48 @@ public class Player : MonoBehaviour
     void Update()
     {
         helper.DoRayCollisionCheck();
-       // if (Input.GetKey("right"))
+        if (Input.GetKey("right"))
         {
-       //     player.velocity = new Vector2(5, 0);
-       //     helper.FlipObject(false);
+            player.velocity = new Vector2(5, 0);
+            helper.FlipObject(false);
         }
-       // if (Input.GetKey("left"))
+        if (Input.GetKey("left"))
         {
-        //    player.velocity = new Vector2(-5, 0);
-         //   helper.FlipObject(true);
+            player.velocity = new Vector2(-5, 0);
+            helper.FlipObject(true);
         }
         // Tells the player to jump if on the ground
-<<<<<<< HEAD
         if (Input.GetButtonDown("space") && grounded)
         {
             player.velocity = new Vector3(player.velocity.x, 9, 0);
             grounded = false;
-=======
-        // if (Input.GetKeyDown("space") && grounded)
-        {
-        //    grounded = false;
-         //   player.velocity = new Vector3(player.velocity.x, 9, 0);
->>>>>>> 2c791981a732517671da32797911d17a69036dc9
-        }
-        // else
-        {
-        //    anim.SetBool("run", player.velocity.magnitude > 0);
-          //  grounded = true;
-        }
-        int moveDirection = 1;
-        if (Input.GetKeyDown("q"))
-        {
-            // Instantiate the fireball at the position and rotation of the player
-            GameObject clone;
-            clone = Instantiate(fireball, transform.position, transform.rotation);
+            if (Input.GetKeyDown("space") && grounded)
+            {
+                grounded = false;
+                player.velocity = new Vector3(player.velocity.x, 9, 0);
+            }
+            else
+            {
+                   anim.SetBool("run", player.velocity.magnitude > 0);
+                  grounded = true;
+            }
+            int moveDirection = 1;
+            if (Input.GetKeyDown("q"))
+            {
+                // Instantiate the fireball at the position and rotation of the player
+                GameObject clone;
+                clone = Instantiate(fireball, transform.position, transform.rotation);
 
-            // get the rigidbody component
-            Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
+                // get the rigidbody component
+                Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
 
-            // set the velocity
-            rb.velocity = new Vector3(15 * moveDirection, 0, 0);
+                // set the velocity
+                rb.velocity = new Vector3(15 * moveDirection, 0, 0);
 
-            //set the position close to the player
-            rb.transform.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z + 1);
-            Object.Destroy(clone.gameObject, 4.0f);
+                //set the position close to the player
+                rb.transform.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z + 1);
+                Object.Destroy(clone.gameObject, 4.0f);
+            }
         }
     }
 }
